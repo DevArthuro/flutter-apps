1e714486baeb258a959072890f963a95fae0ce2b
Carlos Orrego <carlosorrego5151@gmail.com>
2026-01-03 15:36:49 -0500

    feat: add column example

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,17 +1,7 @@
 /**
- * Importaciones propias del lenguaje 
- */
-import 'dart:convert';
-
-/**
  * Importaciones de librerias de terceros 
  */
 import 'package:flutter/material.dart';
-
-/**
- * Importaciones propias 
- */
-import 'package:firstapp/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,9 +12,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: .center,
+            mainAxisAlignment: .spaceAround,
+            mainAxisSize: .max,
+            children: const <Widget>[
+              Column(
+                crossAxisAlignment: .center,
+                mainAxisAlignment: .spaceBetween,
+                mainAxisSize: .max,
+                children: [
+                  Text('We move under cover and we move as one'),
+                  Text('We cannot let a stray gunshot give us away'),
+                  Text("The code word is 'Rochambeau,' dig me?"),
+                  Text('Rochambeau!', style: TextStyle(fontSize: 30.0)),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: .end,
+                mainAxisAlignment: .spaceBetween,
+                mainAxisSize: .max,
+                children: [
+                  Text('We move under cover and we move as one'),
+                  Text('We cannot let a stray gunshot give us away'),
+                  Text("The code word is 'Rochambeau,' dig me?"),
+                  Text('Rochambeau!', style: TextStyle(fontSize: 30.0)),
+                ],
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(title: Text("Hola mundo"), elevation: 10.0),
+      ),
     );
   }
 }
--- firstapp/lib/screens/home_screen.dart
+++ firstapp/lib/screens/home_screen.dart
@@ -5,6 +5,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Center(child: Text("Hola mundo")));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('We move under cover and we move as one'),
+            Text('Through the night, we have one shot to live another day'),
+            Text('We cannot let a stray gunshot give us away'),
+            Text('We will fight up close, seize the moment and stay in it'),
+            Text("It's either that or meet the business end of a bayonet"),
+            Text("The code word is 'Rochambeau,' dig me?"),
+            Text(
+              'Rochambeau!',
+              style: DefaultTextStyle.of(
+                context,
+              ).style.apply(fontSizeFactor: 2.0),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(title: Text("Hola mundo"), elevation: 10.0),
+    );
   }
 }
