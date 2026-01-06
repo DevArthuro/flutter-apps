adfa92737cb7b93213b79a9e4fb4d8d5728d10f6
Carlos Orrego <carlosorrego5151@gmail.com>
2026-01-06 00:03:11 -0500

    feat: add router and default export files

--- flutter_components/lib/main.dart
+++ flutter_components/lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/screens/alert_screen.dart';
+import 'package:flutter_components/screens/card_screen.dart';
+
+import 'package:flutter_components/screens/home_screen.dart';
 import 'package:flutter_components/screens/listview_constructors_screen.dart';
+import 'package:flutter_components/screens/listview_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -11,7 +16,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: ListViewConstructorScreen(),
+      initialRoute: '/list-constructor-view',
+      routes: {
+        '/home': (BuildContext buildContext) => const HomeScreen(),
+        '/alert': (BuildContext buildContext) => const AlertScreen(),
+        '/card': (BuildContext buildContext) => const CardScreen(),
+        '/list-view': (BuildContext buildContext) => const ListViewScreen(),
+        '/list-constructor-view': (BuildContext buildContext) =>
+            const ListViewConstructorScreen(),
+      },
     );
   }
 }
---
+++ flutter_components/lib/screens/alert_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AlertScreen extends StatelessWidget {
+  const AlertScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Initial screen"), centerTitle: true),
+      body: Center(child: Text("Hola mundo")),
+    );
+  }
+}
